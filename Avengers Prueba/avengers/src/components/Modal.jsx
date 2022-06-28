ccac8edd647efa610e68b00edbd24836ba4b72b4
Raul Carbono <carbonoraul2@gmail.com>
2022-06-27 23:22:20 -0500

    avance prueba

--- Avengers Prueba/avengers/src/components/Modal.jsx
+++ Avengers Prueba/avengers/src/components/Modal.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
-const Modal = ({children}) => {
+import '../styles/modal.scss'
+const Modal = ({ data, open, setOpen}) => { 
+    
   return (
-    <article className='modal is-open'>
-       hola
-
+    open &&
+    <article className='modal'>
+        <div className='modal-container'>
+            <h1 className="title">{data.data.name}</h1>
+            <img className="container-image" src={`${data.data.thumbnail.path}.jpg`} alt="" width={500} height={500} />
+            <p className='descripcion'>{data.data.description}</p>
+            <p>{data.data.modified}</p>
+            <button className='modal-close' onClick={()=> setOpen(false)}>X</button>
+         </div>
     </article>
   )
 }
--- Avengers Prueba/avengers/src/components/Personajes.jsx
+++ Avengers Prueba/avengers/src/components/Personajes.jsx
@@ -1,13 +1,19 @@
-import React, {useState, useEffect} from "react";
-//import { infoAvengers } from "../functions/function";
+import React, {useState} from "react";
 import '../styles/style.scss'
+import Modal from "./Modal";
 
 const Personajes = (props) => {
-    
+    const [open, setOpen] = useState(false)
+
     return (
-          <div className="container" onClick={() => localStorage.getItem("datos")}>
+          <>
+          <div className="container" onClick={() => setOpen(true)}>
             <img src={`${props.data.thumbnail.path}.jpg`} alt="" />
+            <h1>{props.data.name}</h1>
          </div>
+         <Modal data={props} open={open} setOpen ={setOpen} />
+         </>
+        
     )
 }
 
