f13de971ed07d6b4a00c7b6932dc0dac2b28bd2d
Raul Carbono <carbonoraul2@gmail.com>
2022-06-28 00:53:03 -0500

    avance prueba

--- Avengers Prueba/src/index.js
+++ Avengers Prueba/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/style.scss';
 
 
@@ -9,10 +9,10 @@ import './styles/style.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
